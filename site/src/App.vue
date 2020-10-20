6d581ec373aab1dd98a2427c16022417cd4b4782
Anthony C. Jones <anthony.jones@msn.com>
2020-10-19 22:30:55 -0500

    adding responsiveness, still need 2 fix mobile

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,25 +1,27 @@
   
 <template>
   <v-app id="inspire">
-    <v-app-bar class='no-print'
-      app
-      color="white"
-      flat
-    >
+    <v-app-bar class="no-print" app color="white" flat hide-on-scroll>
       <v-container class="py-0 fill-height">
-        <router-link
-          class="mr-10"
-          to="/"
-        >
-        <img src="./assets/logo.png"/>
+        <router-link class="mr-10" to="/">
+          <img src="./assets/logo.png" />
         </router-link>
 
-        <v-btn text to="/">
-          Home
-        </v-btn>
-        <v-btn text to="/Rules">
-          Rules
-        </v-btn>
+        <div class="d-none d-sm-block">
+          <v-btn v-for="link in links" :key="link.page" :to="link.route" text>
+            {{ link.page }}
+          </v-btn>
+        </div>
+
+        <div class="d-sm-none">
+          <v-select
+            :items="links"
+            item-text="page"
+            item-value="route"
+            v-model="selectedLink"
+            @change="routeTo()"
+          ></v-select>
+        </div>
 
         <v-spacer></v-spacer>
 
@@ -37,20 +39,33 @@
 
     <v-main class="grey lighten-3">
       <v-container>
-          <router-view></router-view>
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: 'App',
+@Component
+export default class Rules extends Vue {
+  public links = [
+    { page: "Home", route: "/" },
+    { page: "Rules", route: "/Rules" },
+  ];
 
-  data: () => ({
-    //
-  }),
-});
+  public selectedLink = "";
+
+  public routeTo() {
+    if (this.selectedLink) {
+      this.$router.push(this.selectedLink);
+    }
+  }
+
+  public created() {
+    this.selectedLink = this.links[0].route;
+  }
+}
 </script>
