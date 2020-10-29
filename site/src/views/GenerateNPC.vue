849f8410ece7dafd0d345c8ca58cc108ca4dea5e
Anthony Jones <jonesac@smithburgess.net>
2020-10-28 19:09:03 -0500

    Better UI for NPC Generator

--- site/src/views/GenerateNPC.vue
+++ site/src/views/GenerateNPC.vue
@@ -3,18 +3,17 @@
   <div>
     <v-row>
       <v-col cols="12">
-        <v-sheet class="pa-10" rounded="lg">
+        <v-sheet class="pa-10 ma-10" rounded="lg">
           <v-row>
             <v-btn @click="GenerateNPC()">Generate</v-btn>
           </v-row>
           <v-row>
-            <h2>{{ npc.FirstName }}</h2>
-          </v-row>
-          <v-row>
-            <h2>{{ npc.LastName }}</h2>
-          </v-row>
-          <v-row>
-            <h2>{{ Gender }}</h2>
+            <v-card>
+              <v-card-title
+                >{{ npc.FirstName }} {{ npc.LastName }}</v-card-title
+              >
+              <v-card-subtitle>{{ Gender }}</v-card-subtitle>
+            </v-card>
           </v-row>
         </v-sheet>
       </v-col>
@@ -28,13 +27,13 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import * as NPCGenerator from "@/services/NPCGenerator";
 import NPC from "@/models/NPC";
-import { Gender } from '@/enumerations/Gender';
+import { Gender } from "@/enumerations/Gender";
 
 @Component
 export default class GenerateNPC extends Vue {
   public npc = new NPC();
 
-  public get Gender(): string{
+  public get Gender(): string {
     return this.npc.Gender == Gender.Female ? "Female" : "Male";
   }
 
