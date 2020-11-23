974da8a242a1b960e9357e5ea0d6ea2268801451
Anthony C. Jones <anthony.jones@msn.com>
2020-11-22 20:07:55 -0600

    Adding the Malus Line

--- site/src/views/Adventures.vue
+++ site/src/views/Adventures.vue
@@ -2,19 +2,18 @@
 <template>
   <div>
     <v-row>
-      <v-col cols="12">
-        <v-sheet class="pa-10" rounded="lg">
-          <v-row>
-            <p>
+        <v-card class="pa-10" rounded="lg">
+          <v-card-title>
+            Adventures
+          </v-card-title>
+					<v-card-text>
               Below you will find a links to different adventures that can be played with the Terra Machina system.  Though, they are written in such generic way, that they can be easily transferred to any sytem.  They are meant as guiding tools to help the GM run their game.  
-            </p>
-          </v-row>
-
-          <v-row>
-            (In Progress)
-          </v-row>
-        </v-sheet>
-      </v-col>
+					</v-card-text>
+  
+					<v-card-text>
+						<a href="/TheMalusLine.pdf" target="_blank">The Malus Line</a>: The players defy destiny to save a kingdom.
+					</v-card-text>
+        </v-card>
     </v-row>
   </div>
 </template>
